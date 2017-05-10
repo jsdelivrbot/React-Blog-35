a8488aaa73b091806cb413afeaabd70d923b3601
CodeDraken <codedraken@gmail.com>
2017-05-09 22:11:08 -0400

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+      "no-undef": 0,
+      "jsx-a11y/img-has-alt": 0
+    }
+};
